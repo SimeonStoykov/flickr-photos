409fd6f2e8d02f41b76b18bc84e97fc46e22bed8
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2018-12-26 18:50:46 +0200

    Updates

---
+++ precache-manifest.7869c035145133a060863031e4bf76a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "299da546c562d9a35d72e92e8a7ebc64",
+    "url": "/flickr-photos/static/media/magnifier_disabled.299da546.svg"
+  },
+  {
+    "revision": "64d252560740d5a68bab9afcd6b25117",
+    "url": "/flickr-photos/static/media/magnifier.64d25256.svg"
+  },
+  {
+    "revision": "6bbf92206ba0f9cc0988a6d51a1f7e29",
+    "url": "/flickr-photos/static/media/flickr-loading.6bbf9220.svg"
+  },
+  {
+    "revision": "072474462238bab36e39",
+    "url": "/flickr-photos/static/js/runtime~main.07247446.js"
+  },
+  {
+    "revision": "560d67f72de88dab549e",
+    "url": "/flickr-photos/static/js/main.560d67f7.chunk.js"
+  },
+  {
+    "revision": "c2fec1df1fcc771da5c8",
+    "url": "/flickr-photos/static/js/1.c2fec1df.chunk.js"
+  },
+  {
+    "revision": "560d67f72de88dab549e",
+    "url": "/flickr-photos/static/css/main.cffbaab0.chunk.css"
+  },
+  {
+    "revision": "7f86ca9d15392b8fac6de1d18fc5fb0a",
+    "url": "/flickr-photos/index.html"
+  }
+];
