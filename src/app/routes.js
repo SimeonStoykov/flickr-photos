1ca37681b8cd768f165a03e751f16b7e5c7f6477
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2018-12-27 10:12:11 +0200

    style changes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import App from './App';
-import history from './history';
 
 export default () => {
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL} history={history}>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<Switch>
 				<Route exact path="/" component={App} />
 			</Switch>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
